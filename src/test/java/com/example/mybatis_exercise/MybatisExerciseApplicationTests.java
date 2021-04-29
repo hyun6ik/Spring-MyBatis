b2cc18466d96c77a29aa5f21989f2a916da730c7
DESKTOP-NQ23D5Q\Hyunsik <fkanwm@phpschool.com>
2021-04-29 15:52:29 +0900

    프로젝트 생성

---
+++ src/test/java/com/example/mybatis_exercise/MybatisExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybatis_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
